9daf18a4a9d274fa0158f1b7e769a0271fa46897
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-22 11:43:09 +0530

    Palindrome String

---
+++ src/c02_basics/Palindrome.java
@@ -0,0 +1,26 @@
+//To find out whether the given String is Palindrome or not.
+package c02_basics;
+
+import java.util.Scanner;
+
+public class Palindrome {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter a string: ");
+		String str = sc.nextLine();
+
+		String reversed = "";
+
+		for (int i = str.length() - 1; i >= 0; i--) {
+			reversed += str.charAt(i);
+		}
+		
+		if (str.equalsIgnoreCase(reversed)) {
+			System.out.println("The string is a Palindrome.");
+		} else {
+			System.out.println("The string is not a Palindrome.");
+		}
+	}
+
+}
