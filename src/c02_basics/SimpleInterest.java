8b525921f0276aa96e1d74b190b6cf33444c5473
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-22 11:05:42 +0530

    added

---
+++ src/c02_basics/SimpleInterest.java
@@ -0,0 +1,24 @@
+//Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+package c02_basics;
+
+import java.util.Scanner;
+
+public class SimpleInterest {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter principal(P) : ");
+		int principal = sc.nextInt();
+
+		System.out.println("Enter time(T) : ");
+		int time = sc.nextInt();
+
+		System.out.println("Enter rate(R) : ");
+		int rate = sc.nextInt();
+
+		double simpleInterest = ((principal * time * rate) / 100);
+		System.out.println("the Simple Interest is :" + simpleInterest);
+
+	}
+
+}
