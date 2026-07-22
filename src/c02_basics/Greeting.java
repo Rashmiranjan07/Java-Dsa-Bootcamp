94ddec6893a92760f2cbf6f94f8c862ad82c2b07
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-22 10:58:23 +0530

    added

---
+++ src/c02_basics/Greeting.java
@@ -0,0 +1,13 @@
+//Take name as input and print a greeting message for that particular name.
+package c02_basics;
+
+import java.util.Scanner;
+
+public class Greeting {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter you name");
+		String name = sc.nextLine();
+		System.out.println("hello " + name + " ! welcomme ");
+	}
+}
