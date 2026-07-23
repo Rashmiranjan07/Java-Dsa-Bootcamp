062469abbd252521e4740264b766f967ef92eabb
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-23 11:50:57 +0530

    add Areas program

---
+++ src/c03_conditionals_loops/Area.java
@@ -0,0 +1,33 @@
+package c03_conditionals_loops;
+
+import java.util.Scanner;
+
+class AreaCircle {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the radius: ");
+		double radius = sc.nextDouble();
+		double area = Math.PI * radius * radius;
+
+		System.out.println("Area of the circle = " + area);
+	}
+
+}
+
+class AreaTriangle {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the base: ");
+		double base = sc.nextDouble();
+
+		System.out.print("Enter the height: ");
+		double height = sc.nextDouble();
+
+		double area = 0.5 * base * height;
+
+		System.out.println("Area of Triangle = " + area);
+	}
+}
+
+ 
