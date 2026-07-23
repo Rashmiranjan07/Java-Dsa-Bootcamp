0a0e5ed39d634d472ccc613bb04dd3e6740b3579
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-23 11:54:45 +0530

    add Area program

--- src/c03_conditionals_loops/Area.java
+++ src/c03_conditionals_loops/Area.java
@@ -30,4 +30,19 @@ class AreaTriangle {
 	}
 }
 
+class Rectangle{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the width: ");
+		double width = sc.nextDouble();
+
+		System.out.print("Enter the length: ");
+		double length = sc.nextDouble();
+
+		double area = width * length;
+
+		System.out.println("Area of Rectangle = " + area);
+	}
+}
  
