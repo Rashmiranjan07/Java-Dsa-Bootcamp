d6578653cf9231c43c1b1f6529d0dadb42f56269
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-23 12:10:43 +0530

    add Area program

--- src/c03_conditionals_loops/Area.java
+++ src/c03_conditionals_loops/Area.java
@@ -63,4 +63,50 @@ class IsoscelesTriangle{
         System.out.println("Area of Isosceles Triangle = " + area);
 	}
 }
+
+class Parallelogram{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the base: ");
+		double base = sc.nextDouble();
+
+		System.out.print("Enter the height: ");
+		double height = sc.nextDouble();
+
+		double area = base * height;
+
+		System.out.println("Area of Parallelogram = " + area);
+	}
+	
+}
+
+class Rhombus{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the base: ");
+		double base = sc.nextDouble();
+
+		System.out.print("Enter the height: ");
+		double height = sc.nextDouble();
+
+		double area =0.5* base * height;
+
+		System.out.println("Area of Rhombus = " + area);
+	}
+}
  
+class EquilateralTriangle{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the side: ");
+		double side=sc.nextDouble();
+		
+		double area =0.43301270189 * side * side;
+		
+		System.out.println("Area of EquilateralTriangle = " + area);
+
+	}
+}
