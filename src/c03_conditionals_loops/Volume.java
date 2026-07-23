402b18f60b7530f9b6c3dac690a8ba49e0ebddce
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-23 12:47:50 +0530

    Volume java

--- src/c03_conditionals_loops/Volume.java
+++ src/c03_conditionals_loops/Volume.java
@@ -50,3 +50,36 @@ class Cylinder{
 		System.out.println("Volume of Cylinder = " + volume);
 	}
 }
+
+class  Sphere{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the radius: ");
+		double r = sc.nextDouble();
+		
+		double volume= 1.33333333333*Math.PI*r*r*r;
+		
+		System.out.println("Volume of Sphere = " + volume);
+	}
+}
+
+
+class Pyramid{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the length: ");
+		double l = sc.nextDouble();
+		
+		System.out.print("Enter the width: ");
+		double w = sc.nextDouble();
+		
+		System.out.print("Enter the height: ");
+		double h = sc.nextDouble();
+		
+		double volume= (l*w*h)/3;
+		
+		System.out.println("Volume of Pyramid = " + volume);
+	}
+}
