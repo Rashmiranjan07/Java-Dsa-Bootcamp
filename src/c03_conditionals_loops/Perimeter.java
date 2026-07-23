b58bb18ebe7fc01a5a631f928c32bc3cbf9022d8
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-23 12:25:35 +0530

    Perimeter programs

--- src/c03_conditionals_loops/Perimeter.java
+++ src/c03_conditionals_loops/Perimeter.java
@@ -16,32 +16,48 @@ class CirclePerimeter {
 	}
 }
 
-class EquilateralTrianglePerimeter{
+class EquilateralTrianglePerimeter {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		System.out.print("Enter the side: ");
 		double side = sc.nextDouble();
-		
+
 		double perimeter = 3 * side;
-		
-		System.out.println("Perimeter of EquilateralTriangle "+ perimeter);
+
+		System.out.println("Perimeter of EquilateralTriangle " + perimeter);
 
 	}
 }
 
-class Parallelograms{
+class Parallelograms {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("Enter the side: ");
 		double a = sc.nextDouble();
-		
+
 		System.out.print("Enter the side: ");
 		double b = sc.nextDouble();
-		
-		double perimeter = 2 * (a+b);
-		
-		System.out.println("Perimeter of Parallelogram "+ perimeter);
+
+		double perimeter = 2 * (a + b);
+
+		System.out.println("Perimeter of Parallelogram " + perimeter);
+	}
+}
+
+class Rectangles {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the length: ");
+		double l = sc.nextDouble();
+
+		System.out.print("Enter the width: ");
+		double w = sc.nextDouble();
+
+		double perimeter = 2 * (l + w);
+
+		System.out.println("Perimeter of Rectangle " + perimeter);
 	}
 }
