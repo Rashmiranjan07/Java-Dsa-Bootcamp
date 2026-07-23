1b19127fd6b5b2270bd9c8a3eeba4dba0db83a64
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-23 12:42:17 +0530

    Volume program

--- src/c03_conditionals_loops/Volume.java
+++ src/c03_conditionals_loops/Volume.java
@@ -8,13 +8,45 @@ class Cone {
 
 		System.out.print("Enter the base: ");
 		double a = sc.nextDouble();
-		
+
 		System.out.print("Enter the height: ");
 		double b = sc.nextDouble();
-		
-		 double volume = (1.0 / 3.0) * Math.PI * Math.pow(a, 2) * b;
-		 
-		 System.out.println("Volume of Cone = " + volume);
-	}
 
+		double volume = (1.0 / 3.0) * Math.PI * Math.pow(a, 2) * b;
+
+		System.out.println("Volume of Cone = " + volume);
+	}
+}
+
+class Prism {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the base: ");
+		double b = sc.nextDouble();
+
+		System.out.print("Enter the height: ");
+		double h = sc.nextDouble();
+
+		double volume = b * h;
+
+		System.out.println("Volume of Prism = " + volume);
+
+	}
+}
+
+class Cylinder{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the radius: ");
+		double r = sc.nextDouble();
+
+		System.out.print("Enter the height: ");
+		double h = sc.nextDouble();
+		
+		double volume= Math.PI*r*r*h;
+		
+		System.out.println("Volume of Cylinder = " + volume);
+	}
 }
