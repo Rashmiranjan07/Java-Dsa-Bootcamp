b3eff888edb25e7c7f12f7007e4f9bc284459807
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-23 20:10:21 +0530

    Factorial program

---
+++ src/c03_conditionals_loops/Factorial.java
@@ -0,0 +1,19 @@
+//Factorial Program In Java
+
+package c03_conditionals_loops;
+
+import java.util.Scanner;
+
+public class Factorial {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a numbers: ");
+		int n = sc.nextInt();
+		int factorial=1;
+		for ( int i =1;i<= n; i++) {
+		factorial=factorial*i;
+		}
+		System.out.println(factorial);
+	}
+
+}
