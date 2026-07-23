fc8b471782c6a90a5de3c531ad5dc283ea7668b5
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-23 12:21:56 +0530

    Perimeter programs

--- src/c03_conditionals_loops/Perimeter.java
+++ src/c03_conditionals_loops/Perimeter.java
@@ -14,5 +14,34 @@ class CirclePerimeter {
 		System.out.println("Perimeter (Circumference) of the Circle = " + perimeter);
 
 	}
+}
 
+class EquilateralTrianglePerimeter{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the side: ");
+		double side = sc.nextDouble();
+		
+		double perimeter = 3 * side;
+		
+		System.out.println("Perimeter of EquilateralTriangle "+ perimeter);
+
+	}
+}
+
+class Parallelograms{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the side: ");
+		double a = sc.nextDouble();
+		
+		System.out.print("Enter the side: ");
+		double b = sc.nextDouble();
+		
+		double perimeter = 2 * (a+b);
+		
+		System.out.println("Perimeter of Parallelogram "+ perimeter);
+	}
 }
