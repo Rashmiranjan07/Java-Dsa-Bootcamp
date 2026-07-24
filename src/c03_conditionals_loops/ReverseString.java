85fea0c23330a1a91470962711965decf6e104ad
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-24 10:45:07 +0530

    Reverse a String

---
+++ src/c03_conditionals_loops/ReverseString.java
@@ -0,0 +1,20 @@
+//Reverse A String In Java
+
+package c03_conditionals_loops;
+
+import java.util.Scanner;
+
+public class ReverseString {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter :");
+		String original = sc.nextLine();
+		String reversed = "";
+
+		for (int i = original.length() - 1; i >= 0; i--) {
+			reversed = reversed + original.charAt(i);
+		}
+		System.out.println("The reverse string is :" + reversed);
+	}
+
+}
