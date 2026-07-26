a88696da9558b1a45c1ab1146744311564e9afb3
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-26 18:59:26 +0530

    Find the maximum integer

--- src/c05_arrays/MaxItem.java
+++ src/c05_arrays/MaxItem.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 public class MaxItem {
 	public static void main(String[] args) {
 		int[] arr = { 1, 3, 23, 9, 18 };
-
 		System.out.println("the maximum value is " + isMax(arr));
 
 	}
