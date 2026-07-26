59db6407d4bde5f7341f3a381bfba7cad70a3b9e
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-26 18:40:27 +0530

    Arraylist

--- src/c05_arrays/ArrayList.java
+++ src/c05_arrays/ArrayList.java
@@ -1,15 +1,28 @@
 package c05_arrays;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class Demo {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		// Syntax
 		ArrayList<Integer> list = new ArrayList<>(10);
-		
-		list.add(67);
-		list.add(98);
-		
+//		
+//		list.add(67);
+//		list.add(98);
+//		
+//		System.out.println(list);
+//
+		// input
+		for (int i = 0; i < 5; i++) {
+			list.add(sc.nextInt());
+		}
+		// get item at any index
+		for (int i = 0; i < 5; i++) {
+			System.out.println(list.get(i));
+		}
+
 		System.out.println(list);
 	}
 
