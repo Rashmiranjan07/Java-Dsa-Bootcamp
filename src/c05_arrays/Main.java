01c0e389163743ef6d1ea0bb17f469941a974337
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-26 18:23:17 +0530

    1D array & 2D array

--- src/c05_arrays/Main.java
+++ src/c05_arrays/Main.java
@@ -25,8 +25,54 @@ class Main {
 		for (int i = 0; i < str.length; i++) {
 			str[i] = sc.next();
 		}
-		
+
 		System.out.println(Arrays.toString(str));
 	}
 
+}
+
+class TwoD {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int[][] arr = new int[3][3];
+		System.out.println(arr.length);
+
+		for (int row = 0; row < arr.length; row++) {
+			// for each col in every row
+			for (int col = 0; col < arr[row].length; col++) {
+				arr[row][col] = sc.nextInt();
+			}
+		}
+
+		// output
+//		for (int row = 0; row < arr.length; row++) {
+//			// for each col in every row
+//			for (int col = 0; col < arr[row].length; col++) {
+//				System.out.print(arr[row][col] + " ");
+//			}
+//			System.out.println();
+//		}
+		for (int row = 0; row < arr.length; row++) {
+			// for each col in every row
+			System.out.print(Arrays.toString(arr[row]));
+		}
+		System.out.println();
+	}
+}
+
+class twoDD {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int[][] arr = { { 1, 2, 3, 4 }, { 5, 6 }, { 7, 8, 9 } };
+		for (int row = 0; row < arr.length; row++) {
+
+			for (int col = 0; col < arr[row].length; col++) {
+				System.out.print(arr[row][col] + " ");
+			}
+			System.out.println();
+		}
+
+	}
 }
