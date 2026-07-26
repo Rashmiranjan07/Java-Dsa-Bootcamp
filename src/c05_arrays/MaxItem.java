38aac19e563171190c5369a6d81ef10ff98c093c
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-26 19:05:44 +0530

    Find the maximum integer

--- src/c05_arrays/MaxItem.java
+++ src/c05_arrays/MaxItem.java
@@ -13,6 +13,16 @@ public class MaxItem {
 
 	// FOund the maximum item in a range
 	static int maxRange(int[] arr, int start, int end) {
+
+		// Work on edge cases
+		if (end > start) {
+			return -1;
+		}
+
+		if (arr == null) {
+			return -1;
+		}
+
 		int maxVal = arr[0];
 		for (int i = start; i <= end; i++) {
 			if (arr[i] > maxVal) {
@@ -24,6 +34,11 @@ public class MaxItem {
 	}
 
 	static int isMax(int[] arr) {
+
+		// work on edge cases
+		if (arr.length == 0) {
+			return -1;
+		}
 		int maxVal = arr[0];
 		for (int i = 1; i < arr.length; i++) {
 			if (arr[i] > maxVal) {
