f960fbc2c70511408bfb61e4522d38210d10d28a
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-26 18:31:10 +0530

    Arraylist

---
+++ src/c05_arrays/ArrayList.java
@@ -0,0 +1,16 @@
+package c05_arrays;
+
+import java.util.ArrayList;
+
+class Demo {
+	public static void main(String[] args) {
+		// Syntax
+		ArrayList<Integer> list = new ArrayList<>(10);
+		
+		list.add(67);
+		list.add(98);
+		
+		System.out.println(list);
+	}
+
+}
