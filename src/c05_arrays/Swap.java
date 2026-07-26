75d4d66ec15c40fa89796b05a8f2d17975f21c5e
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-26 18:53:14 +0530

    Swap numbers

---
+++ src/c05_arrays/Swap.java
@@ -0,0 +1,19 @@
+package c05_arrays;
+
+import java.util.Arrays;
+
+public class Swap {
+	public static void main(String[] args) {
+		int[] arr = { 1, 3, 23, 9, 18 };
+		swap(arr, 1, 3);
+		System.out.println(Arrays.toString(arr));
+	}
+
+	static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+
+	}
+
+}
