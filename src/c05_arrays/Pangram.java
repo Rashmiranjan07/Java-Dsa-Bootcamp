5d7690ae57c3be60d85ff6e7049fd2d8dcf67998
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-28 12:04:31 +0530

    Check Pangram

---
+++ src/c05_arrays/Pangram.java
@@ -0,0 +1,25 @@
+/* Check if the sentense is pangram or not ?
+ -------------------------------------------
+ A pangram is a sentence where every letter of the English alphabet appears at least once.
+Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+Example 1:
+Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+Output: true
+Explanation: sentence contains at least one of every letter of the English alphabet.
+
+ */
+
+package c05_arrays;
+
+public class Pangram {
+	public static void main(String[] args) {
+
+	}
+
+	public boolean checkIfPangram(String sentence) {
+		return false;
+
+	}
+
+}
