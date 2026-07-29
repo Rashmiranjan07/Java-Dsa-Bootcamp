f42347819b9616e2f6bc6fffccac32e065ae3b66
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-29 12:59:54 +0530

    Sprial matrix

--- src/c05_arrays/SpiralMatrix.java
+++ src/c05_arrays/SpiralMatrix.java
@@ -1,8 +1,32 @@
+/* Spiral matrix
+ * --------------
+ Given an m x n matrix, return all elements of the matrix in spiral order.
+Example 1:
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+Example 2:
+Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+ */
+
 package c05_arrays;
+
+import java.util.List;
 
 public class SpiralMatrix {
 	public static void main(String[] args) {
-		
+		int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+		SpiralMatrix obj = new SpiralMatrix();
+
+		List<Integer> result = obj.spiralOrder(matrix);
+
+		System.out.println(result);
+	}
+
+	public List<Integer> spiralOrder(int[][] matrix) {
+		return null;
+
 	}
 
 }
