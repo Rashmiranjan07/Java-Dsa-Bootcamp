3a909de74d3cf51cf4c57484a9ee088916e9c805
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-29 13:06:08 +0530

    Sprial matrix

--- src/c05_arrays/SpiralMatrix.java
+++ src/c05_arrays/SpiralMatrix.java
@@ -11,6 +11,7 @@ Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 
 package c05_arrays;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SpiralMatrix {
@@ -25,13 +26,49 @@ public class SpiralMatrix {
 	}
 
 	public List<Integer> spiralOrder(int[][] matrix) {
-		   List<Integer> result = new ArrayList<>();
+		List<Integer> result = new ArrayList<>();
 
-	        if (matrix == null || matrix.length == 0) {
-	            return result;
-	        }
-	        
-	        
+		if (matrix == null || matrix.length == 0) {
+			return result;
+		}
+		int m = matrix.length - 1;
+		int n = matrix[0].length - 1;
+
+		int top = 0;
+		int bottom = m;
+		int left = 0;
+		int right = n;
+		while (top <= bottom && left <= right) {
+
+			// Traverse Left to Right
+			for (int i = left; i <= right; i++) {
+				result.add(matrix[top][i]);
+			}
+			top++;
+
+			// Traverse Top to Bottom
+			for (int i = top; i <= bottom; i++) {
+				result.add(matrix[i][right]);
+			}
+			right--;
+
+			// Traverse Right to Left
+			if (top <= bottom) {
+				for (int i = right; i >= left; i--) {
+					result.add(matrix[bottom][i]);
+				}
+				bottom--;
+			}
+
+			// Traverse Bottom to Top
+			if (left <= right) {
+				for (int i = bottom; i >= top; i--) {
+					result.add(matrix[i][left]);
+				}
+				left++;
+			}
+		}
+
 		return null;
 
 	}
