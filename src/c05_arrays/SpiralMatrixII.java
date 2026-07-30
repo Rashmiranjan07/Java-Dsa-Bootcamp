105500be0540a1d8fb8f2e01edf1686619c55a03
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-30 11:35:01 +0530

    Spiral matrix

--- src/c05_arrays/SpiralMatrixII.java
+++ src/c05_arrays/SpiralMatrixII.java
@@ -12,13 +12,64 @@ Output: [[1]]
 
 package c05_arrays;
 
+import java.util.List;
+
 public class SpiralMatrixII {
 	public static void main(String[] args) {
+		int[][] matrix = { { 1, 2, 3 },
+				           { 4, 5, 6 }, 
+				           { 7, 8, 9 } 
+				           };
+
+		SpiralMatrix obj = new SpiralMatrix();
+		List<Integer> result = obj.spiralOrder(matrix);
+		System.out.println(result);
 
 	}
 
 	public int[][] generateMatrix(int n) {
-		return null;
+
+		int[][] matrix = new int[n][n];
+
+		int top = 0;
+		int bottom = n - 1;
+		int left = 0;
+		int right = n - 1;
+
+		int num = 1;
+
+		while (top <= bottom && left <= right) {
+
+			// Fill top row (Left -> Right)
+			for (int j = left; j <= right; j++) {
+				matrix[top][j] = num++;
+			}
+			top++;
+
+			// Fill right column (Top -> Bottom)
+			for (int i = top; i <= bottom; i++) {
+				matrix[i][right] = num++;
+			}
+			right--;
+
+			// Fill bottom row (Right -> Left)
+			if (top <= bottom) {
+				for (int j = right; j >= left; j--) {
+					matrix[bottom][j] = num++;
+				}
+				bottom--;
+			}
+
+			// Fill left column (Bottom -> Top)
+			if (left <= right) {
+				for (int i = bottom; i >= top; i--) {
+					matrix[i][left] = num++;
+				}
+				left++;
+			}
+		}
+
+		return matrix;
 
 	}
 
