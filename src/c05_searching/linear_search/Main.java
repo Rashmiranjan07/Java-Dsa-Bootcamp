db610aab12677de1d6ba1420d09705e6aec6b31f
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-31 13:02:49 +0530

    Added binary search

--- src/c05_searching/linear_search/Main.java
+++ src/c05_searching/linear_search/Main.java
@@ -3,9 +3,28 @@ package c05_searching.linear_search;
 public class Main {
 	public static void main(String[] args) {
 		int[] nums = { 23, 45, 1, 2, 8, 19, -3, 16, -11, 28 };
-		int target = 100;
+		int target = 19;
 		int ans = linearSearch(nums, target);
 		System.out.println(ans);
+
+	}
+
+	// search the target and return the element
+	static int linearSearch2(int[] arr, int target) {
+		if (arr.length == 0) {
+			return -1;
+		}
+
+		// run a for loop
+		for (int element : arr) {
+			if (element == target) {
+				return element;
+			}
+		}
+
+		// this line will excute if none of the return statements above have excuted
+		// hence the target not found
+		return -1;
 
 	}
 
