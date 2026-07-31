af6f4c614c0b40b83f9a4066a1ca91e0471e6875
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-31 10:29:50 +0530

    Jump game

--- src/c05_arrays/JumpGame.java
+++ src/c05_arrays/JumpGame.java
@@ -2,7 +2,24 @@ package c05_arrays;
 
 public class JumpGame {
 	public static void main(String[] args) {
-		
+
+	}
+
+	public boolean canJump(int[] nums) {
+		int maxReach = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+
+			// If the current index cannot be reached
+			if (i > maxReach) {
+				return false;
+			}
+
+			// Update the farthest index we can reach
+			maxReach = Math.max(maxReach, i + nums[i]);
+		}
+
+		return true;
 	}
 
 }
