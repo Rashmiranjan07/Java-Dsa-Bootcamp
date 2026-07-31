6dec46dd70dfe065115c94ece83589ceced3ffc5
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-31 21:47:29 +0530

    Learing linear search practice

--- src/c05_searching/linear_search/Main.java
+++ src/c05_searching/linear_search/Main.java
@@ -88,15 +88,38 @@ class SearchInString {
 	}
 }
 
-/* search in a range 
- * arr=[18,12,-7,3,14,28]
+/*
+ * search in a range arr=[18,12,-7,3,14,28]
  * 
  * Search for 3' in the range of index[1 & 4]
  * 
-*/
-class SearchRange{
+ */
+class SearchRange {
 	public static void main(String[] args) {
-		
+		int arr[] = { 18, 12, -7, 3, 14, 28 };
+		int target = 3;
+		System.out.println(linearSearch(arr, target, 1, 4));
 	}
-	
+
+	static int linearSearch(int[] arr, int target, int start, int end) {
+		if (arr.length == 0) {
+			return -1;
+		}
+
+		// run a for loop
+		for (int i = start; i <= end; i++) {
+			// check for element at every index if it is = target
+			int element = arr[i];
+
+			if (element == target) {
+				return i;
+			}
+		}
+
+		// this line will excute if none of the return statements above have excuted
+		// hence the target not found
+		return -1;
+
+	}
+
 }
