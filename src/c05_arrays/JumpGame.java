d147093bc7776e16d1b40514322c8643f592566d
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-07-31 10:48:38 +0530

    Jump game

--- src/c05_arrays/JumpGame.java
+++ src/c05_arrays/JumpGame.java
@@ -7,7 +7,7 @@ Input: nums = [2,3,1,1,4]
 Output: true
 Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
 Example 2:
-Input: nums = [3,2,1,0,4]
+Input: nums =  [3,2,1,0,4]
 Output: false
 Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
  
@@ -17,6 +17,8 @@ package c05_arrays;
 
 public class JumpGame {
 	public static void main(String[] args) {
+		int [] nums= {2,3,1,1,4};
+		
 
 	}
 
