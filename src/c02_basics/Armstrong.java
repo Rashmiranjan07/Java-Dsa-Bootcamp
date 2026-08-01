85441fca78c0ef7721e310f936fb21c7e360b232
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-01 19:14:47 +0530

    Check armstrong number in between two numbers

--- src/c02_basics/Armstrong.java
+++ src/c02_basics/Armstrong.java
@@ -9,36 +9,43 @@ public class Armstrong {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("Enter start number : ");
+		System.out.print("Enter start number: ");
 		int start = sc.nextInt();
 
-		System.out.println("Enter end number: ");
+		System.out.print("Enter end number: ");
 		int end = sc.nextInt();
+
 		for (int num = start; num <= end; num++) {
 			int original = num;
 			int temp = num;
 			int digits = 0;
 			int sum = 0;
 
-			// count the digits
+			// Count the number of digits
 			while (temp != 0) {
 				digits++;
 				temp /= 10;
 			}
 
-			// Reset temp
+			// Handle 0 separately
+			if (original == 0) {
+				digits = 1;
+			}
+
 			temp = original;
 
 			// Calculate Armstrong sum
 			while (temp > 0) {
 				int digit = temp % 10;
-				sum += Math.pow(digit, digits);
-				temp = temp / 10;
+				sum += (int) Math.pow(digit, digits);
+				temp /= 10;
 			}
 
 			if (sum == original) {
 				System.out.println(original);
 			}
 		}
+
+		sc.close();
 	}
 }
