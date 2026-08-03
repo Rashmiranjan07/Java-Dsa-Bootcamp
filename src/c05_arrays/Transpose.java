678f110e3ae1aeab11afc9d6077978d46e206478
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-03 12:58:26 +0530

    Transpose array

--- src/c05_arrays/Transpose.java
+++ src/c05_arrays/Transpose.java
@@ -2,10 +2,15 @@ package c05_arrays;
 
 public class Transpose {
 	public static void main(String[] args) {
+		int [][] matrix = { {1,2,3},
+				            {4,5,6},
+				            {7,8,9} };
+		}
 
-	}
+	
 
 	public int[][] transpose(int[][] matrix) {
+		
 		return matrix;
 
 	}
