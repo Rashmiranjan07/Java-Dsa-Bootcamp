7078d9427ed44195c7fb35fc3da9b6de2cb9856f
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-04 13:19:41 +0530

    Binary Search

--- src/c05_searching/binary_search/Main.java
+++ src/c05_searching/binary_search/Main.java
@@ -3,7 +3,7 @@ package c05_searching.binary_search;
 public class Main {
 	public static void main(String[] args) {
 		int[] arr = { -18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89 };
-		int target = 22;
+		int target = 17;
 		int ans = binarySearch(arr, target);
 		System.out.println(ans);
 
