36a5af600828ac765366659d2930f722d5b28d2a
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-04 19:35:09 +0530

    Find smallest letter greater than target

---
+++ src/c05_searching/binary_search/NextGreatestNumber.java
@@ -0,0 +1,39 @@
+/*744. Find smallest letter greater than target
+ * ---------------------------------------
+You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+Example 1:
+
+Input: letters = ["c","f","j"], target = "a"
+Output: "c"
+Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
+Example 2:
+
+Input: letters = ["c","f","j"], target = "c"
+Output: "f"
+Explanation: The smallest character that is lexicographically greater than 'c' in letters is 'f'.
+Example 3:
+
+Input: letters = ["x","x","y","y"], target = "z"
+Output: "x"
+Explanation: There are no characters in letters that is lexicographically greater than 'z' so we return letters[0].
+ */
+
+
+
+package c05_searching.binary_search;
+
+public class NextGreatestNumber {
+	public static void main(String[] args) {
+		char [] letters= {'c','f','j'};
+		char target= 'a';
+
+	}
+
+	public char nextGreatestLetter(char[] letters, char target) {
+		
+		return target;
+
+	}
+
+}
