abf943c15d19679d352250e87e0a3e0f87da4cb7
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-04 13:04:07 +0530

    Celling target

---
+++ src/c05_searching/binary_search/Ceiling.java
@@ -0,0 +1,18 @@
+
+
+
+package c05_searching.binary_search;
+
+public class Ceiling {
+	public static void main(String[] args) {
+		int arr []= {2,3,5,9,14,16,18};
+		int target=15;
+		
+	}
+	static int isCeiling(int arr[], int target) {
+		
+		return target;
+		
+	}
+
+}
