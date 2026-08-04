0112f37bdb5df998cb9996671f15a5fbabbd9d0f
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-04 13:39:06 +0530

    Ceiling program

--- src/c05_searching/binary_search/Ceiling.java
+++ src/c05_searching/binary_search/Ceiling.java
@@ -4,19 +4,36 @@
  
  */
 
-
 package c05_searching.binary_search;
 
 public class Ceiling {
 	public static void main(String[] args) {
-		int arr []= {2,3,5,9,14,16,18};
-		int target=15;
-		
+		int arr[] = { 2, 3, 5, 9, 14, 16, 18 };
+		int target = 15;
+		int ans = isCeiling(arr, target);
+		System.out.println(ans);
+
 	}
+     
+	// return the index of smallest number >=target
 	static int isCeiling(int arr[], int target) {
-		
-		return target;
-		
+		int start = 0;
+		int end = arr.length - 1;
+		while (start <= end) {
+			
+			int mid = start + (end - start) / 2;
+
+			if (target < arr[mid]) {
+				end = mid - 1;
+			} else if (target > arr[mid]) {
+				start = mid + 1;
+			} else {
+				
+				return mid;
+			}
+		}
+		return start;
+
 	}
 
 }
