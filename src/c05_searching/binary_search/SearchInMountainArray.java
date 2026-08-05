511e035963aaa6767e69ba70e33e1d8802fc0ed2
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-05 20:27:39 +0530

    Search in Mountain array

--- src/c05_searching/binary_search/SearchInMountainArray.java
+++ src/c05_searching/binary_search/SearchInMountainArray.java
@@ -2,7 +2,11 @@ package c05_searching.binary_search;
 
 public class SearchInMountainArray {
 	public static void main(String[] args) {
-		
+
+	}
+
+	public int findInMountainArray(int target, MountainArray mountainArr) {
+
 	}
 
 }
