cab6bc4cad648728a1a912d426cccc9c6c042b20
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-05 13:47:39 +0530

    find the position of element in an infinite array

---
+++ src/c05_searching/binary_search/InfiniteArray.java
@@ -0,0 +1,29 @@
+package c05_searching.binary_search;
+
+public class InfiniteArray {
+	public static void main(String[] args) {
+		
+	}
+	
+	static int ans(int[]arr, int target, int start,int end) {
+		// first s
+	}
+
+	static int binarySearch(int[] arr, int target, int start, int end) {
+		
+		while (start <= end) {
+
+			int mid = start + (end - start) / 2;
+
+			if (target < arr[mid]) {
+				end = mid - 1;
+			} else if (target > arr[mid]) {
+				start = mid + 1;
+			} else {
+				// ans found
+				return mid;
+			}
+		}
+		return -1;
+	}
+}
