472c8c0d9ed6e7fd30e5ed2f6271cec28e6da21e
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-05 11:49:47 +0530

    Find the First & Last element of an sorted array

---
+++ src/c05_searching/binary_search/FirstAndLastPositionOfElement.java
@@ -0,0 +1,12 @@
+package c05_searching.binary_search;
+
+public class FirstAndLastPositionOfElement {
+	public static void main(String[] args) {
+
+	}
+
+	public int[] searchRange(int[] nums, int target) {
+
+	}
+
+}
