705e8facea774e19d9261bdddc3b1cd5e6319adf
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-05 20:25:22 +0530

    find the peak index in a mountian array

--- src/c05_searching/binary_search/PeakIndexInMountainArray.java
+++ src/c05_searching/binary_search/PeakIndexInMountainArray.java
@@ -1,4 +1,4 @@
-/* 852. Peak index in a mountian array 
+/* 852. & 162. Peak index in a mountian array 
  * -----------------------------------
 You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
 Return the index of the peak element.
