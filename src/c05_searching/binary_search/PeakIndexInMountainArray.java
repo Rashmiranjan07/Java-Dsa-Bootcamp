68ff48c101913f5c67d76cc84c93ad7c15f0dbbc
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-05 20:14:57 +0530

    Find position of an element in a sorted array

--- src/c05_searching/binary_search/PeakIndexInMountainArray.java
+++ src/c05_searching/binary_search/PeakIndexInMountainArray.java
@@ -1,38 +1,27 @@
 /* 852. Peak index in a mountian array 
  * -----------------------------------
 You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
-
 Return the index of the peak element.
-
 Your task is to solve it in O(log(n)) time complexity.
 
- 
-
 Example 1:
-
 Input: arr = [0,1,0]
-
 Output: 1
 
 Example 2:
-
 Input: arr = [0,2,1,0]
-
 Output: 1
 
 Example 3:
-
 Input: arr = [0,10,5,2]
-
 Output: 1
  */
-
 
 package c05_searching.binary_search;
 
 public class PeakIndexInMountainArray {
 	public static void main(String[] args) {
-		int[] arr = { 0, 1,2,3,2,1, 0 };
+		int[] arr = { 0, 1, 2, 3, 2, 1, 0 };
 		PeakIndexInMountainArray obj = new PeakIndexInMountainArray();
 		int result = obj.peakIndexInMountainArray(arr);
 		System.out.println(result);
