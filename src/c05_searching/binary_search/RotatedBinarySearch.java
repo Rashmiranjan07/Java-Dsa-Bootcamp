799e8da04b3b9f4ac0d5a02df338cf9651798699
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-06 13:22:10 +0530

    Search in rotated array

--- src/c05_searching/binary_search/RotatedBinarySearch.java
+++ src/c05_searching/binary_search/RotatedBinarySearch.java
@@ -4,11 +4,12 @@ package c05_searching.binary_search;
 public class RotatedBinarySearch {
 	public static void main(String[] args) {
 		int[] nums = { 4, 5, 6, 7, 0, 1, 2 };
+		//System.out.println(findPivot(nums));
 	}
 
-	public int search(int[] nums, int target) {
-
-	}
+//	public int search(int[] nums, int target) {
+//                                               
+//	}
 
 	static int findPivot(int[] nums) {
 
@@ -16,8 +17,19 @@ public class RotatedBinarySearch {
 		int end = nums.length - 1;
 		while (start <= end) {
 			int mid = start + (end - start) / 2;
+			if (mid < end && nums[mid] > nums[mid + 1]) {
+				return mid;
+			}
+			if (mid > start && nums[mid] < nums[mid - 1]) {
+				return mid - 1;
+			}
+			if (nums[mid] <= nums[start]) {
+				end = mid - 1;
+			} else {
+				start = mid + 1;
+			}
 		}
-
+		return -1;
 	}
 
 }
