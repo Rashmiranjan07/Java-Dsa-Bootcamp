63873d58f9db128f303f96bd996e0886358a05d0
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-06 18:24:21 +0530

    Search in rotated array

--- src/c06_searching/binary_search/RotatedBinarySearch.java
+++ src/c06_searching/binary_search/RotatedBinarySearch.java
@@ -75,7 +75,8 @@ public class RotatedBinarySearch {
 	static int binarySearch(int[] arr, int target, int start, int end) {
 		while (start <= end) {
 			// find the middle element
-            // int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+			// int mid = (start + end) / 2; // might be possible that (start + end) exceeds
+			// the range of int in java
 			int mid = start + (end - start) / 2;
 
 			if (target < arr[mid]) {
@@ -89,5 +90,27 @@ public class RotatedBinarySearch {
 		}
 		return -1;
 	}
+
+	// this will not work in duplicate values
+//    static int findPivot(int[] arr) {
+//        int start = 0;
+//        int end = arr.length - 1;
+//        while (start <= end) {
+//            int mid = start + (end - start) / 2;
+//            // 4 cases over here
+//            if (mid < end && arr[mid] > arr[mid + 1]) {
+//                return mid;
+//            }
+//            if (mid > start && arr[mid] < arr[mid - 1]) {
+//                return mid-1;
+//            }
+//            if (arr[mid] <= arr[start]) {
+//                end = mid - 1;
+//            } else {
+//                start = mid + 1;
+//            }
+//        }
+//        return -1;
+//    }
 
 }
