45666e0005dd0dd279fa468e6775d76c9481805a
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-06 13:28:43 +0530

    Search in rotated array

--- src/c05_searching/binary_search/RotatedBinarySearch.java
+++ src/c05_searching/binary_search/RotatedBinarySearch.java
@@ -1,15 +1,37 @@
+/* 33. Search in rotated array 
 
+ */
 package c05_searching.binary_search;
 
 public class RotatedBinarySearch {
 	public static void main(String[] args) {
 		int[] nums = { 4, 5, 6, 7, 0, 1, 2 };
-		//System.out.println(findPivot(nums));
+		int target = 1;
+		RotatedBinarySearch obj = new RotatedBinarySearch();
+		int result = obj.search(nums, target);
+		System.out.println(result);
 	}
 
-//	public int search(int[] nums, int target) {
-//                                               
-//	}
+	public int search(int[] nums, int target) {
+		int pivot = findPivot(nums);
+
+		// if you did not find a pivot, it means the array is not rotated
+		if (pivot == -1) {
+			// just do normal binary search
+			return binarySearch(nums, target, 0, nums.length - 1);
+		}
+
+		// if pivot is found, you have found 2 asc sorted arrays
+		if (nums[pivot] == target) {
+			return pivot;
+		}
+
+		if (target >= nums[0]) {
+			return binarySearch(nums, target, 0, pivot - 1);
+		}
+
+		return binarySearch(nums, target, pivot + 1, nums.length - 1);
+	}
 
 	static int findPivot(int[] nums) {
 
@@ -27,6 +49,24 @@ public class RotatedBinarySearch {
 				end = mid - 1;
 			} else {
 				start = mid + 1;
+			}
+		}
+		return -1;
+	}
+
+	static int binarySearch(int[] arr, int target, int start, int end) {
+		while (start <= end) {
+			// find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+			int mid = start + (end - start) / 2;
+
+			if (target < arr[mid]) {
+				end = mid - 1;
+			} else if (target > arr[mid]) {
+				start = mid + 1;
+			} else {
+				// ans found
+				return mid;
 			}
 		}
 		return -1;
