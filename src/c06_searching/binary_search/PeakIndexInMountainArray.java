ee1b95c0c19406b3a7eaf33f3d5286f65ae407b7
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-06 13:47:06 +0530

    Peak index in a mountain

--- src/c06_searching/binary_search/PeakIndexInMountainArray.java
+++ src/c06_searching/binary_search/PeakIndexInMountainArray.java
@@ -1,5 +1,5 @@
 /* 852. & 162. Peak index in a mountian array 
- * -----------------------------------
+ ---------------------------------------------
 You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
 Return the index of the peak element.
 Your task is to solve it in O(log(n)) time complexity.
