2f72c19885e407ebbc41da6679c5b549950fb730
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-07 12:23:32 +0530

    Split array largest sum

--- src/c06_searching/binary_search/SplitArrayLargestSum.java
+++ src/c06_searching/binary_search/SplitArrayLargestSum.java
@@ -1,4 +1,5 @@
 /*410. Split array largest sum ( hard)
+ * ----------------------------------
 Given an integer array nums and an integer k, split nums into k non-empty subarrays such that the largest sum of any subarray is minimized.
 Return the minimized largest sum of the split.
 A subarray is a contiguous part of the array.
