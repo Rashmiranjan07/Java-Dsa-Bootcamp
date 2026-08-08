40c89a0a7ba3d1da1347684c1c915c825fc0a18a
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-08 12:36:26 +0530

    Find the valid perfect square

--- src/c06_searching/binary_search/PerfectSquare.java
+++ src/c06_searching/binary_search/PerfectSquare.java
@@ -1,12 +1,8 @@
 /* 367. Valid perfect square 
   ---------------------------
-  Given a positive integer num, return true if num is a perfect square or false otherwise.
-
+Given a positive integer num, return true if num is a perfect square or false otherwise.
 A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
-
 You must not use any built-in library function, such as sqrt.
-
- 
 
 Example 1:
 
