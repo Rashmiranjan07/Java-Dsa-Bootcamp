46e50f17a0a62c9d3b80a2009b8636f2d886138d
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-08 12:56:40 +0530

    finding the Kth missing positive number

---
+++ src/c06_searching/binary_search/KthMissingPositiveNumber.java
@@ -0,0 +1,14 @@
+package c06_searching.binary_search;
+
+public class KthMissingPositiveNumber {
+	public static void main(String[] args) {
+
+	}
+
+	public int findKthPositive(int[] arr, int k) {
+		
+		return k;
+
+	}
+
+}
