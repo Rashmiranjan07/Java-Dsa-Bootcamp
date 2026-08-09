27aac2c00d6a4e6528dddba2ebf16b5b4441e0d6
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-09 18:05:01 +0530

    Binary search

---
+++ src/c06_searching/binary_search/BinarySearch.java
@@ -0,0 +1,28 @@
+package c06_searching.binary_search;
+
+public class BinarySearch {
+	public static void main(String[] args) {
+
+	}
+
+	public int search(int[] nums, int target) {
+		int start = 0;
+		int end = nums.length - 1;
+
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+
+			if (target < nums[mid]) {
+				end = mid - 1;
+			} else if (target > nums[mid]) {
+				start = mid + 1;
+			} else {
+				return mid;
+			}
+		}
+
+		return -1;
+
+	}
+
+}
