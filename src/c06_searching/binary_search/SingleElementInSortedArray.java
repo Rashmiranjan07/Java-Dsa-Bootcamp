6099d691f975867ead6c0e3375c6cd3a76e9f284
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-10 12:07:52 +0530

    Find the single non-duplicate element in an sorted array

---
+++ src/c06_searching/binary_search/SingleElementInSortedArray.java
@@ -0,0 +1,15 @@
+package c06_searching.binary_search;
+
+public class SingleElementInSortedArray {
+	public static void main(String[] args) {
+		int arr []= { 1,1,2,3,3,4,4,8,8};
+				
+		
+
+	}
+
+	public int singleNonDuplicate(int[] nums) {
+
+	}
+
+}
