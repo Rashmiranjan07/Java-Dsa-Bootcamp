75e68ed9f83a9438ef847fc8932e6ed102a41458
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-10 12:50:42 +0530

    Find the single non-duplicate element in an sorted array

--- src/c06_searching/binary_search/SingleElementInSortedArray.java
+++ src/c06_searching/binary_search/SingleElementInSortedArray.java
@@ -1,3 +1,19 @@
+/*540. Single Element in a Sorted Array ( medium )
+----------------------------------------
+You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+Return the single element that appears only once.
+Your solution must run in O(log n) time and O(1) space.
+
+Example 1:
+Input: nums = [1,1,2,3,3,4,4,8,8]
+Output: 2
+
+Example 2:
+Input: nums = [3,3,7,7,10,11,11]
+Output: 10
+
+ */
+
 package c06_searching.binary_search;
 
 public class SingleElementInSortedArray {
@@ -11,31 +27,44 @@ public class SingleElementInSortedArray {
 	}
 
 	public int singleNonDuplicate(int[] nums) {
+
 		int start = 0;
 		int n = nums.length - 1;
 		int end = n;
 
+		// Only one element
+		if (nums.length == 1)
+			return nums[0];
+
 		while (start <= end) {
+
 			int mid = start + (end - start) / 2;
 
-			if (n == 1)
-				return nums[0];
+			// First element
 			if (mid == 0 && nums[0] != nums[1])
 				return nums[mid];
-			if (mid == n - 1 && nums[n - 1] != nums[n - 2])
+
+			// Last element
+			if (mid == n && nums[n - 1] != nums[n])
 				return nums[mid];
 
+			// Single element is in the middle
 			if (nums[mid - 1] != nums[mid] && nums[mid] != nums[mid + 1]) {
 				return nums[mid];
 			}
 
+			// mid is even
 			if (mid % 2 == 0) {
+
 				if (nums[mid - 1] == nums[mid]) {
 					end = mid - 1;
 				} else {
 					start = mid + 1;
 				}
+
+				// mid is odd
 			} else {
+
 				if (nums[mid - 1] == nums[mid]) {
 					start = mid + 1;
 				} else {
@@ -43,6 +72,7 @@ public class SingleElementInSortedArray {
 				}
 			}
 		}
+
 		return -1;
 	}
 }
