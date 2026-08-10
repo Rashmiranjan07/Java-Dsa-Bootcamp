3b371859d6f99284eb5aa045dab8864799b2c3b3
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-10 12:44:06 +0530

    Find the single non-duplicate element in an sorted array

--- src/c06_searching/binary_search/SingleElementInSortedArray.java
+++ src/c06_searching/binary_search/SingleElementInSortedArray.java
@@ -2,7 +2,7 @@ package c06_searching.binary_search;
 
 public class SingleElementInSortedArray {
 	public static void main(String[] args) {
-		int nums[] = { 1, 1, 2, 3, 3, 4, 4, 8, 8 };
+		int nums[] = { 1, 1, 2, 2, 3, 4, 4, 8, 8 };
 
 		SingleElementInSortedArray obj = new SingleElementInSortedArray();
 		int result = obj.singleNonDuplicate(nums);
@@ -12,28 +12,36 @@ public class SingleElementInSortedArray {
 
 	public int singleNonDuplicate(int[] nums) {
 		int start = 0;
-		int end = nums.length;
+		int n = nums.length - 1;
+		int end = n;
 
 		while (start <= end) {
 			int mid = start + (end - start) / 2;
-			if (end % 2 == 0) {
-				if (nums[mid - 1] == nums[mid] && nums[mid] == nums[mid + 1]) {
-					return mid;
-				} else if (nums[mid - 1] == nums[mid]) {
+
+			if (n == 1)
+				return nums[0];
+			if (mid == 0 && nums[0] != nums[1])
+				return nums[mid];
+			if (mid == n - 1 && nums[n - 1] != nums[n - 2])
+				return nums[mid];
+
+			if (nums[mid - 1] != nums[mid] && nums[mid] != nums[mid + 1]) {
+				return nums[mid];
+			}
+
+			if (mid % 2 == 0) {
+				if (nums[mid - 1] == nums[mid]) {
 					end = mid - 1;
-				} else if (nums[mid] == nums[mid + 1]) {
+				} else {
 					start = mid + 1;
 				}
 			} else {
-				if (nums[mid - 1] == nums[mid] && nums[mid] == nums[mid + 1]) {
-					return mid;
-				} else if (nums[mid] == nums[mid + 1]) {
-					end = mid - 1;
-				} else if (nums[mid - 1] == nums[mid]) {
+				if (nums[mid - 1] == nums[mid]) {
 					start = mid + 1;
+				} else {
+					end = mid - 1;
 				}
 			}
-
 		}
 		return -1;
 	}
