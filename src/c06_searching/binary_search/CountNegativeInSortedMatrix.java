6296033222aaa4ed836a0620620ea708a5760388
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-10 11:01:54 +0530

    Counting negative numbers in sorted matrix (r)

--- src/c06_searching/binary_search/CountNegativeInSortedMatrix.java
+++ src/c06_searching/binary_search/CountNegativeInSortedMatrix.java
@@ -2,17 +2,32 @@ package c06_searching.binary_search;
 
 public class CountNegativeInSortedMatrix {
 	public static void main(String[] args) {
-		int[][] grid = { { 4, 3, 2, -1 }, 
-				         { 3, 2, 1, -1 }, 
-				         { 1, 1, -1, -2 }, 
-				         { -1, -1, -2, -3 } };
+		int[][] grid = { { 4, 3, 2, -1 }, { 3, 2, 1, -1 }, { 1, 1, -1, -2 }, { -1, -1, -2, -3 } };
 		CountNegativeInSortedMatrix obj = new CountNegativeInSortedMatrix();
+		int result = obj.countNegatives(grid);
+		System.out.println(result);
 
 	}
 
 	public int countNegatives(int[][] grid) {
 
-		return 0;
+		int count = 0;
+		int n = grid[0].length;
+
+		for (int[] row : grid) {
+			int start = 0;
+			int end = n - 1;
+			while (start <= end) {
+				int mid = start + (end - start) / 2;
+				if (row[mid] < 0) {
+					end = mid - 1;
+				} else {
+					start = mid + 1;
+				}
+			}
+			count += n - start;
+		}
+		return count;
 	}
 
 }
