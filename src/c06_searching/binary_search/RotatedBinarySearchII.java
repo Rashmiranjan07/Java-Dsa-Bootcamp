f46f09a378430b3422873c13200f9e0b054df455
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-11 12:16:09 +0530

    Search in a rotated array II

--- src/c06_searching/binary_search/RotatedBinarySearchII.java
+++ src/c06_searching/binary_search/RotatedBinarySearchII.java
@@ -18,6 +18,11 @@ package c06_searching.binary_search;
 
 public class RotatedBinarySearchII {
 	public static void main(String[] args) {
+		int[] nums = { 2, 5, 6, 0, 0, 1, 2 };
+		int target = 9;
+		RotatedBinarySearchII obj = new RotatedBinarySearchII();
+		boolean result = obj.search(nums, target);
+		System.out.println(result);
 
 	}
 
