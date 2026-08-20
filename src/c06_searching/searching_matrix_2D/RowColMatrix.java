6f1cb3aad6cf32fad421c3830297dc04adf80c97
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-20 13:09:33 +0530

    Search in matrix / 2D arrays

--- src/c06_searching/searching_matrix_2D/RowColMatrix.java
+++ src/c06_searching/searching_matrix_2D/RowColMatrix.java
@@ -2,6 +2,13 @@ package c06_searching.searching_matrix_2D;
 
 public class RowColMatrix {
 	public static void main(String[] args) {
+		int[][] matrix = { 
+				{ 10, 20, 30, 40 },
+				{ 15, 25, 35, 45 }, 
+				{ 28, 29, 37, 49 }, 
+				{ 33, 34, 38, 50 }
+		};
+		int target = 37;
 
 	}
 
