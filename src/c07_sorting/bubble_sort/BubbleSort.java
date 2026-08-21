83d4145977871df8187e1966eff418512264e0cb
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-21 11:56:59 +0530

    Bubble Sort

--- src/c07_sorting/bubble_sort/BubbleSort.java
+++ src/c07_sorting/bubble_sort/BubbleSort.java
@@ -11,7 +11,12 @@ public class BubbleSort {
 			// for each step, max item will come at the last respective index
 			for( int j=1; j<arr.length-i;j++) {
 				// swap if the item smaller than the previous item
-				
+				if(arr[j]<arr[j-1]) {
+					//swap
+					int temp=arr[j];
+					arr[j]=arr[j-1];
+					arr[j-1]= temp;
+				}
 			}
 			
 		}
