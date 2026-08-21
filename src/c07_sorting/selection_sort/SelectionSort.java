8eb84e94fa56a08dd0d15ebcfca4c585a2d0a91b
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-21 23:23:45 +0530

    Selection Sort

---
+++ src/c07_sorting/selection_sort/SelectionSort.java
@@ -0,0 +1,10 @@
+// Selection sort :- Select an element ( max/min) and put it on its correct index .
+
+package c07_sorting.selection_sort;
+
+public class SelectionSort {
+	public static void main(String[] args) {
+		
+	}
+
+}
