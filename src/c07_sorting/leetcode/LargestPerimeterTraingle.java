ff1985aacefb01dbf263782e1550baf17781bc3f
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-21 19:42:34 +0530

    Largest Perimeter Traingle

--- src/c07_sorting/leetcode/LargestPerimeterTraingle.java
+++ src/c07_sorting/leetcode/LargestPerimeterTraingle.java
@@ -1,13 +1,13 @@
 /* 976. Largest Perimeter Triangle ( easy )
  -------------------------------------------
  Given an integer array nums, return the largest perimeter of a triangle with a non-zero area, formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
+ 
 Example 1:
-
 Input: nums = [2,1,2]
 Output: 5
 Explanation: You can form a triangle with three side lengths: 1, 2, and 2.
-Example 2:
 
+Example 2:
 Input: nums = [1,2,1,10]
 Output: 0
 Explanation: 
@@ -34,7 +34,7 @@ public class LargestPerimeterTraingle {
 
 	public int largestPerimeter(int[] nums) {
 		for (int i = 0; i < nums.length - 1; i++) {
-			for (int j = 1; j < nums.length - 1; j++) {
+			for (int j = 1; j < nums.length - i; j++) {
 				if (nums[j] < nums[j - 1]) {
 					int temp = nums[j];
 					nums[j] = nums[j - 1];
