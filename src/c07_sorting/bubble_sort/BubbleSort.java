d30ee3c5c104b6a222d962fe4dac33df35e5361d
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-21 11:59:27 +0530

    Bubble Sort

--- src/c07_sorting/bubble_sort/BubbleSort.java
+++ src/c07_sorting/bubble_sort/BubbleSort.java
@@ -1,24 +1,28 @@
 package c07_sorting.bubble_sort;
 
+import java.util.Arrays;
+
 public class BubbleSort {
 	public static void main(String[] args) {
-		
+		int[] arr = { 3, 5, 6, 4, 2, 1 };
+		bubbleSort(arr);
+		System.out.println(Arrays.toString(arr));
 	}
-	
-	static void bubbleSort(int[]arr) {
+
+	static void bubbleSort(int[] arr) {
 		// run the steps n-1 times
-		for(int i=0; i<arr.length; i++) {
+		for (int i = 0; i < arr.length; i++) {
 			// for each step, max item will come at the last respective index
-			for( int j=1; j<arr.length-i;j++) {
+			for (int j = 1; j < arr.length - i; j++) {
 				// swap if the item smaller than the previous item
-				if(arr[j]<arr[j-1]) {
-					//swap
-					int temp=arr[j];
-					arr[j]=arr[j-1];
-					arr[j-1]= temp;
+				if (arr[j] < arr[j - 1]) {
+					// swap
+					int temp = arr[j];
+					arr[j] = arr[j - 1];
+					arr[j - 1] = temp;
 				}
 			}
-			
+
 		}
 	}
 
