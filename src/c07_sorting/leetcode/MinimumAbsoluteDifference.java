989001e193903ae7fd7b4b8c1a5ec25ca7302a14
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-21 19:22:23 +0530

    Minimum absolute difference

--- src/c07_sorting/leetcode/MinimumAbsoluteDifference.java
+++ src/c07_sorting/leetcode/MinimumAbsoluteDifference.java
@@ -1,3 +1,29 @@
+/* 1200. Minimum absolute difference (easy)
+ *-----------------------------------------
+Given an array of distinct integers arr, find all pairs of elements with the minimum absolute difference of any two elements.
+Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows
+a, b are from arr
+a < b
+b - a equals to the minimum absolute difference of any two elements in arr
+
+Example 1:
+
+Input: arr = [4,2,1,3]
+Output: [[1,2],[2,3],[3,4]]
+Explanation: The minimum absolute difference is 1. List all pairs with difference equal to 1 in ascending order.
+Example 2:
+
+Input: arr = [1,3,6,10,15]
+Output: [[1,3]]
+Example 3:
+
+Input: arr = [3,8,-10,23,19,-4,-14,27]
+Output: [[-14,-10],[19,23],[23,27]]
+ 
+
+ */
+
+
 package c07_sorting.leetcode;
 
 import java.util.ArrayList;
@@ -6,13 +32,9 @@ import java.util.List;
 public class MinimumAbsoluteDifference {
 
 	public static void main(String[] args) {
-
 		int[] arr = { 4, 2, 1, 3 };
-
 		MinimumAbsoluteDifference obj = new MinimumAbsoluteDifference();
-
 		List<List<Integer>> answer = obj.minimumAbsDifference(arr);
-
 		System.out.println(answer);
 	}
 
