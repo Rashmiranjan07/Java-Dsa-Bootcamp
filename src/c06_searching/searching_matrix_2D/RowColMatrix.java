8da6c145db3dfa053ed75642cfa4140605d99fea
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-21 10:54:06 +0530

    Binary search in sorted matrix/2D arrays

--- src/c06_searching/searching_matrix_2D/RowColMatrix.java
+++ src/c06_searching/searching_matrix_2D/RowColMatrix.java
@@ -6,20 +6,25 @@ import java.util.Arrays;
 
 public class RowColMatrix {
 	public static void main(String[] args) {
-		int[][] matrix = { 
-				{ 10, 20, 30, 40 },
-				{ 15, 25, 35, 45 }, 
-				{ 28, 29, 37, 49 }, 
-				{ 33, 34, 38, 50 }
-		};
-		int target = 37;
-		System.out.println(Arrays.toString(search(matrix, target)));
+//		int[][] matrix = { 
+//				{ 10, 20, 30, 40 },
+//				{ 15, 25, 35, 45 }, 
+//				{ 28, 29, 37, 49 }, 
+//				{ 33, 34, 38, 50 }
+//		};
+		int[][] arr = { 
+				{ 1, 2, 3 }, 
+				{ 4, 5, 6 }, 
+				{ 7, 8, 9 } 
+				};
+		int target = 6;
+		System.out.println(Arrays.toString(search(arr, target)));
 
 	}
 
 	static int[] search(int[][] matrix, int target) {
 		int r = 0;
-		int c = matrix.length - 1;
+		int c = matrix[0].length-1;
 
 		while (r < matrix.length && c >= 0) {
 			if (matrix[r][c] == target) {
