9ebf25b03b177d5d646c620a7c090d36fa2387cc
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-22 17:15:48 +0530

    Sort Array By Parity

--- src/c07_sorting/leetcode/SortArrayByParity.java
+++ src/c07_sorting/leetcode/SortArrayByParity.java
@@ -1,5 +1,5 @@
-/*905. Sort Array By Parity (easy)
- ---------------------------------
+/* 905. Sort Array By Parity (easy)
+---------------------------------
 Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
 Return any array that satisfies this condition.
 
