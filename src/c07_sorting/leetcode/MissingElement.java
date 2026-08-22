7eea3ef8f482d7f58fcf698612d47871727ec976
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-23 00:04:50 +0530

    Missing Element

--- src/c07_sorting/leetcode/MissingElement.java
+++ src/c07_sorting/leetcode/MissingElement.java
@@ -1,9 +1,6 @@
 /*268. Missing Number (easy)
  * -------------------------
 iven an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
-
- 
-
 Example 1:
 
 Input: nums = [3,0,1]
