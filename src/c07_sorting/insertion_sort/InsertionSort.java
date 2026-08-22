fcf5b9cac7a6e64f3fd707e82ec081d5b54bacda
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-22 20:53:25 +0530

    Insertion Sort

---
+++ src/c07_sorting/insertion_sort/InsertionSort.java
@@ -0,0 +1,14 @@
+package c07_sorting.insertion_sort;
+
+public class InsertionSort {
+	public static void main(String[] args) {
+		int [] arr = {4,5,1,2,3};
+		
+	}
+	
+	static void insertion(int [] arr) {
+		
+	}
+	
+
+}
