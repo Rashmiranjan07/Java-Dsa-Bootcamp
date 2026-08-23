a4b7fd37b7c603c43717f579bf7914d6af7de3f3
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-23 20:17:59 +0530

    Find All Numbers Disaappered in an Array (Goggle)

--- src/c07_sorting/leetcode/DisapperedNumInArray.java
+++ src/c07_sorting/leetcode/DisapperedNumInArray.java
@@ -1,4 +1,15 @@
-// Goggle 
+/* 448. Find All Numbers Disappeared in an Array ( easy ) ( Asked in Goggle )
+ ----------------------------------------------------------------------------
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+Example 1:
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+
+Example 2:
+Input: nums = [1,1]
+Output: [2]
+ */
 
 package c07_sorting.leetcode;
 
@@ -44,7 +55,11 @@ public class DisapperedNumInArray {
 		arr[second] = temp;
 	}
 }
+
+
 /*
- * Tips :- ------- 1. If range =[0 - n] evry element will be at index=value 2.
- * If range = [1,n] every element will be at index=value-1
+ * Tips :-
+ *  ------- 
+ *  1. If range =[0 - n] evry element will be at index=value 
+ *  2. If range = [1,n] every element will be at index=value-1
  */
