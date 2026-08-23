39da40524a9048a8233ed395f3541c09bb22dd29
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-23 19:53:04 +0530

    Missing element (Amazon)

--- src/c07_sorting/leetcode/MissingElementII.java
+++ src/c07_sorting/leetcode/MissingElementII.java
@@ -34,7 +34,6 @@ public class MissingElementII {
 		int i = 0;
 		while (i < arr.length) {
 			int correct = arr[i];
-			;
 			if (arr[i] < arr.length && arr[i] != arr[correct]) {
 				swap(arr, i, correct);
 			} else {
