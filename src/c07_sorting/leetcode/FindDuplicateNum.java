6d040e6c61cbceb74bc27505d60ce86b39bf55ff
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-23 10:43:38 +0530

    Find out the Duplicate element

--- src/c07_sorting/leetcode/FindDuplicateNum.java
+++ src/c07_sorting/leetcode/FindDuplicateNum.java
@@ -1,34 +1,31 @@
 /* 287. Find the Duplicate Number (medium)
- *----------------------------------------
- *Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
-
+ ----------------------------------------
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
 There is only one repeated number in nums, return this repeated number.
-
 You must solve the problem without modifying the array nums and using only constant extra space.
 
- 
-
 Example 1:
-
 Input: nums = [1,3,4,2,2]
 Output: 2
-Example 2:
 
+Example 2:
 Input: nums = [3,1,3,4,2]
 Output: 3
-Example 3:
 
+Example 3:
 Input: nums = [3,3,3,3,3]
 Output: 3
 
  */
 
-
 package c07_sorting.leetcode;
 
 public class FindDuplicateNum {
 	public static void main(String[] args) {
-		int [] nums= {};
+		int[] nums = { 1, 3, 4, 2, 2 };
+		FindDuplicateNum obj = new FindDuplicateNum();
+		int result = obj.findDuplicate(nums);
+		System.out.println(result);
 
 	}
 
