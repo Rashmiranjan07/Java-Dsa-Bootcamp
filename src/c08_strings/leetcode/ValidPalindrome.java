9f7619cd88a9df7466cf6b44ab747b2627a40705
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-26 12:37:37 +0530

    Valid palindrome(r

---
+++ src/c08_strings/leetcode/ValidPalindrome.java
@@ -0,0 +1,53 @@
+/* 125. Valid Palindrome (Easy)
+-------------------------------
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+Example 3:
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+
+ */
+
+
+package c08_strings.leetcode;
+
+public class ValidPalindrome {
+	public static void main(String[] args) {
+		String str = "cac";
+		ValidPalindrome obj = new ValidPalindrome();
+		boolean result = obj.isPalindrome(str);
+		System.out.println(result);
+
+	}
+
+	public boolean isPalindrome(String str) {
+		if (str == null || str.length() == 0) {
+			return true;
+		}
+		str = str.toLowerCase();
+		for (int i = 0; i <= str.length() / 2; i++) {
+			char start = str.charAt(i);
+			char end = str.charAt(str.length() - 1 - i);
+
+			if (start != end) {
+				return false;
+			}
+		}
+		return true;
+
+	}
+
+}
