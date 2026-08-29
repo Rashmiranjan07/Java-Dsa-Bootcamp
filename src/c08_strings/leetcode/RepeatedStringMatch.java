8592096c8e646789711fe4bc1318bfbe767ca608
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-29 18:30:41 +0530

    Repeated String match

--- src/c08_strings/leetcode/RepeatedStringMatch.java
+++ src/c08_strings/leetcode/RepeatedStringMatch.java
@@ -7,6 +7,20 @@ public class RepeatedStringMatch {
 	}
 
 	public int repeatedStringMatch(String a, String b) {
+		String temp = a;
+		int count = 1;
+
+		while (temp.length() < b.length()) {
+			temp = temp + a;
+			count++;
+		}
+		if (temp.contains(b)) {
+			return count;
+		} else if ((temp + a).contains(b)) {
+			return count;
+		}
+
+		return -1;
 
 	}
 
