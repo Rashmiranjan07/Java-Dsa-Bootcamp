c4fb295160273e29987a6984262b7a4f7590aca2
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-29 12:44:18 +0530

    Binary Search in Recursion

--- src/c10_recursion/questions/BinarySearch.java
+++ src/c10_recursion/questions/BinarySearch.java
@@ -3,6 +3,9 @@ package c10_recursion.questions;
 public class BinarySearch {
 	public static void main(String[] args) {
 
+		int[] arr = { 1, 2, 3, 4, 55, 66, 78 };
+		int target = 4;
+		System.out.println(search(arr, target, 0, arr.length - 1));
 	}
 
 	static int search(int[] arr, int target, int s, int e) {
