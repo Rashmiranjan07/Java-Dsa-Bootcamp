ec4aefe825399d1eef56a08e2958a99481ce6af4
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-29 10:20:56 +0530

    Recursion learning

---
+++ src/c10_recursion/code/NumberExampleRecursion.java
@@ -0,0 +1,17 @@
+package c10_recursion.code;
+
+public class NumberExampleRecursion {
+	public static void main(String[] args) {
+
+	}
+
+	static void printNum(int n) {
+		if (n == 5) {
+			System.out.println(5);
+			return;
+		}
+		System.out.println(n);
+		printNum(n + 1);
+	}
+
+}
