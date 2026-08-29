74432a3232b4c5e458c94ad9a08860594f3e9733
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-29 12:37:58 +0530

    Binary Search in Recursion

---
+++ src/c10_recursion/questions/BinarySearch.java
@@ -0,0 +1,15 @@
+package c10_recursion.questions;
+
+public class BinarySearch {
+	public static void main(String[] args) {
+
+	}
+
+	static int search(int[] arr, int target, int s, int e) {
+		if (s > e) {
+			return -1;
+		}
+		int m = s + (e - s) / 2;
+	}
+
+}
