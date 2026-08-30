a6f9a8e3ef2cd0c9ce61dccd0589d5277f1544a6
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-30 10:51:25 +0530

    Multiply strings

--- src/c08_strings/leetcode/MultiplyStrings.java
+++ src/c08_strings/leetcode/MultiplyStrings.java
@@ -12,16 +12,36 @@ public class MultiplyStrings {
 
 	public String multiply(String num1, String num2) {
 
-		int n1 = Integer.parseInt(num1); // 2
-		int n2 = Integer.parseInt(num2); // 3
+		if (num1.equals("0") || num2.equals("0")) {
+			return "0";
+		}
 
-		int mult = 1;
-		mult = n1 * n2;
+		int[] result = new int[num1.length() + num2.length()];
 
-		String s = String.valueOf(mult);
+		for (int i = num1.length() - 1; i >= 0; i--) {
+			for (int j = num2.length() - 1; j >= 0; j--) {
 
-		return s;
+				int a = num1.charAt(i) - '0';
+				int b = num2.charAt(j) - '0';
 
+				int pos = i + j + 1;
+
+				int total = a * b + result[pos];
+
+				result[pos] = total % 10;
+				result[pos - 1] += total / 10;
+			}
+		}
+
+		StringBuilder ans = new StringBuilder();
+
+		for (int x : result) {
+			if (ans.length() == 0 && x == 0) {
+				continue;
+			}
+			ans.append(x);
+		}
+
+		return ans.toString();
 	}
-
 }
