1d0eb70d990867f2ba47e87fc8b1c213055f6bb1
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-08-31 12:11:58 +0530

    Integer to Roman

---
+++ src/c12_math/questions/IntegerToRoman.java
@@ -0,0 +1,28 @@
+package c12_math.questions;
+
+public class IntegerToRoman {
+	public static void main(String[] args) {
+		int num = 3749;
+		IntegerToRoman obj = new IntegerToRoman();
+		String result = obj.intToRoman(num);
+		System.out.println(result);
+	}
+
+	public String intToRoman(int num) {
+		int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+
+		String[] symbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+		StringBuilder result = new StringBuilder();
+
+		for (int i = 0; i < values.length; i++) {
+			while (num >= values[i]) {
+				num -= values[i];
+				result.append(symbols[i]);
+			}
+		}
+
+		return result.toString();
+
+	}
+}
