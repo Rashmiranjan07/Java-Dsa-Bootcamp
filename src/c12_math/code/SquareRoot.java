3c4e20fa7f738918c778c6f2d9de1471421e8ec1
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-02 10:26:48 +0530

    Finding Square Root of a number

---
+++ src/c12_math/code/SquareRoot.java
@@ -0,0 +1,16 @@
+//Finding Square Root of a Number
+
+package c12_math.code;
+
+public class SquareRoot {
+	public static void main(String[] args) {
+		int n = 40;
+		int p = 3;
+		System.out.println(sqrt(n, p));
+	}
+
+	static double sqrt(int n, int p) {
+
+	}
+
+}
