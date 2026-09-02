aa327788eefe3f7949fc954a994dcf33a7abc0bc
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-02 11:25:48 +0530

    Number of Digit one

--- src/c12_math/questions/CountDigitOne.java
+++ src/c12_math/questions/CountDigitOne.java
@@ -1,3 +1,16 @@
+/* 233. Number of Digit One (Hard)
+  -------------------------------
+Given an integer n, count the total number of digit 1 appearing in all non-negative integers less than or equal to n.
+
+Example 1:
+Input: n = 13
+Output: 6
+
+Example 2:
+Input: n = 0
+Output: 0
+ */
+
 package c12_math.questions;
 
 public class CountDigitOne {
