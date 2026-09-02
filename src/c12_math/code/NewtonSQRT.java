e1c3996b384d33754f5ef4fb9b1818e4efe09d6a
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-02 11:27:33 +0530

    Newton Raphson Method

---
+++ src/c12_math/code/NewtonSQRT.java
@@ -0,0 +1,28 @@
+/* Newton Raphson Method
+--------------------------
+root = X + N/2
+
+*/
+
+package c12_math.code;
+
+public class NewtonSQRT {
+	public static void main(String[] args) {
+		System.out.println(sqrt(40));
+	}
+
+	static double sqrt(double n) {
+		double x = n;
+		double root;
+		while (true) {
+			root = 0.5 * (x + (n / x));
+
+			if (Math.abs(root - x) < 0.5) {
+				break;
+			}
+
+			x = root;
+		}
+		return root;
+	}
+}
