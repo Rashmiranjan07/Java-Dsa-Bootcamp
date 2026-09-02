4f169085c098eef0fa1c35e067f24ffc90af8caf
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-02 11:47:57 +0530

    Permutation Sequence

--- src/c12_math/questions/PermutationSequence.java
+++ src/c12_math/questions/PermutationSequence.java
@@ -1,12 +1,6 @@
-/*
-60. Permutation Sequence
-Solved
-Hard
-Topics
-premium lock icon
-Companies
+/* 60. Permutation Sequence (Hard)
+  -------------------------------
 The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
-
 By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
 
 "123"
@@ -17,22 +11,18 @@ By listing and labeling all of the permutations in order, we get the following s
 "321"
 Given n and k, return the kth permutation sequence.
 
- 
-
 Example 1:
-
 Input: n = 3, k = 3
 Output: "213"
-Example 2:
 
+Example 2:
 Input: n = 4, k = 9
 Output: "2314"
-Example 3:
 
+Example 3:
 Input: n = 3, k = 1
 Output: "123"
  */
-
 
 package c12_math.questions;
 
