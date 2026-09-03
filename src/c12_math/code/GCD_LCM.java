ce797d5e1e030d60814d796ad6498b7ba8b430ba
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-03 10:40:57 +0530

    GCD & LCM

--- src/c12_math/code/GCD_LCM.java
+++ src/c12_math/code/GCD_LCM.java
@@ -1,8 +1,14 @@
-package c12_math.code;
+/* GCD & LCM 
+ -------------
+HCF = Highest Common Factor
+GCD = Greatest Common Divisor
+ */
+
 
 public class GCD_LCM {
 	public static void main(String[] args) {
 		
 	}
+
 
 }
