ef1f51d67ddfed63168935edb303d9ec5c8ca067
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-03 10:31:05 +0530

    Modulo

--- src/c12_math/code/Modulo.java
+++ src/c12_math/code/Modulo.java
@@ -1,5 +1,16 @@
+/* Modulo
+  -------
+1. (a+b)%m=((a%m) + (b%m))%m
+2. (a-b)%m=((a%m) - (b%m)+m)%m
+3. (a*b)%m=((a%m) * (b%m))%m
+
+ */
+
 package c12_math.code;
 
 public class Modulo {
+	public static void main(String[] args) {
+		
+	}
 
 }
