0af5a7f2415e640983e89eac27d7ab32aae7d3c3
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-03 10:49:18 +0530

    GCD & LCM

--- src/c12_math/code/GCD_LCM.java
+++ src/c12_math/code/GCD_LCM.java
@@ -1,4 +1,5 @@
-/* GCD & LCM 
+/*
+GCD & LCM 
  -------------
 GCD/HCF
 ------- 
@@ -8,15 +9,29 @@ ex:- (4,18)
 4=1,2,4
 18=1,2,3,6,9,18
 HCF / GCD = 2
-
-
  */
-
+package c12_math.code;
 
 public class GCD_LCM {
+
 	public static void main(String[] args) {
-		
+
+		int x = 4;
+		int y = 18;
+
+		int result = hcf(x, y);
+
+		System.out.println("GCD = " + result);
 	}
 
+	static int hcf(int x, int y) {
 
+		while (y != 0) {
+			int temp = y;
+			y = x % y;
+			x = temp;
+		}
+
+		return x;
+	}
 }
