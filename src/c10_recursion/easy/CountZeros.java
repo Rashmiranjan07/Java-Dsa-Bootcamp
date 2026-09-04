7814b2ef8137f55c7eabc3b2b744297cb7d4db1c
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-04 11:48:26 +0530

    Count Number of Zeros in a number

---
+++ src/c10_recursion/easy/CountZeros.java
@@ -0,0 +1,12 @@
+// Count Number of Zeros in a number 
+
+package c10_recursion.easy;
+
+public class CountZeros {
+	public static void main(String[] args) {
+		
+	}
+	static int helper(int n) {
+		
+	}
+}
