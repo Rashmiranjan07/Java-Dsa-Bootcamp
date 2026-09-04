52b41ed55f5cf0d6af3d9364935cb3c768a044ee
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-04 11:24:11 +0530

    Reverse a Number by Recursion

---
+++ src/c10_recursion/easy/ReverseNum.java
@@ -0,0 +1,24 @@
+// Reverse a Number 
+
+
+package c10_recursion.easy;
+
+public class ReverseNum {
+
+	static int sum = 0;
+
+	static void rev1(int n) {
+		if (n == 0) {
+			return;
+		}
+		int rem = n % 10;
+		sum = sum * 10 + rem;
+		rev1(n / 10);
+	}
+
+	public static void main(String[] args) {
+		rev1(1234);
+		System.out.println(sum);
+	}
+
+}
