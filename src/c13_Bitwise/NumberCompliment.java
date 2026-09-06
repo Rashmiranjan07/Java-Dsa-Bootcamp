65ff6bad09c4548c1f6b37403c87456bbe508b9a
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-06 17:35:43 +0530

    Number Compliment

--- src/c13_Bitwise/NumberCompliment.java
+++ src/c13_Bitwise/NumberCompliment.java
@@ -3,10 +3,21 @@ package c13_Bitwise;
 public class NumberCompliment {
 	public static void main(String[] args) {
 		int num = 5;
+		NumberCompliment obj = new NumberCompliment();
+		int result = obj.findComplement(num);
+		System.out.println(result);
 	}
 
 	public int findComplement(int num) {
+		int mask = 0;
+		int temp = num;
 
+		while (temp > 0) {
+			mask = (mask << 1) | 1;
+			temp >>= 1;
+		}
+
+		return num ^ mask;
 	}
 
 }
