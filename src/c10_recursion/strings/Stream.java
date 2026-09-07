a7c224e3ad5448921a9f8adc50132fd12240a4ae
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-07 18:19:04 +0530

    Skip a Character

--- src/c10_recursion/strings/Stream.java
+++ src/c10_recursion/strings/Stream.java
@@ -21,4 +21,18 @@ public class Stream {
 		}
 	}
 
+	static String skip(String up) {
+		if (up.isEmpty()) {
+			return "";
+		}
+
+		char ch = up.charAt(0);
+
+		if (ch == 'a') {
+			return skip(up.substring(1));
+		} else {
+			return ch + skip(up.substring(1));
+		}
+	}
+
 }
