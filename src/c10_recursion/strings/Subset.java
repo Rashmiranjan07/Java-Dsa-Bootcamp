c6e39f5f44a6ae656bdc18660163246c6341661c
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-07 18:49:10 +0530

    Subsequence

--- src/c10_recursion/strings/Subset.java
+++ src/c10_recursion/strings/Subset.java
@@ -3,9 +3,12 @@
 
 package c10_recursion.strings;
 
+import java.util.ArrayList;
+
 public class Subset {
 	public static void main(String[] args) {
-		subseq("", "abc");
+//		subseq("", "abc");
+		System.out.println(subseqRet("", "abc"));
 	}
 
 	static void subseq(String p, String up) {
@@ -17,6 +20,22 @@ public class Subset {
 		char ch = up.charAt(0);
 		subseq(p, up.substring(1));
 		subseq(p + ch, up.substring(1));
+	}
+
+	// what if we added in an array List
+
+	static ArrayList<String> subseqRet(String p, String up) {
+		if (up.isEmpty()) {
+			ArrayList<String> list = new ArrayList<>();
+			list.add(p);
+			return list;
+		}
+		char ch = up.charAt(0);
+		ArrayList<String> left = subseqRet(p + ch, up.substring(1));
+		ArrayList<String> right = subseqRet(p, up.substring(1));
+
+		left.addAll(right);
+		return left;
 	}
 
 }
