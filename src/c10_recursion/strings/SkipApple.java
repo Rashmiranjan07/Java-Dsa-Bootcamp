7f03cdd99f174505a67899490bc527c7097c7209
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-07 18:22:55 +0530

    Skip a string using recursion

--- src/c10_recursion/strings/SkipApple.java
+++ src/c10_recursion/strings/SkipApple.java
@@ -1,5 +1,36 @@
+// Skip a string :-
+/*
+String = bdapplefg 
+o/p- bdfg
+ */
+
 package c10_recursion.strings;
 
 public class SkipApple {
+	public static void main(String[] args) {
+
+	}
+
+	static String skipApple(String up) {
+		if (up.isEmpty()) {
+			return "";
+		}
+		if (up.startsWith("apple")) {
+			return skipApple(up.substring(5));
+		} else {
+			return up.charAt(0) + skipApple(up.substring(1));
+		}
+	}
+
+	static String skipAppNotApple(String up) {
+		if (up.isEmpty()) {
+			return "";
+		}
+		if (up.startsWith("app") && !up.startsWith("apple")) {
+			return skipAppNotApple(up.substring(3));
+		} else {
+			return up.charAt(0) + skipAppNotApple(up.substring(1));
+		}
+	}
 
 }
