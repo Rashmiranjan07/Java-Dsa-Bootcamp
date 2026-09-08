a54c41ae45fbd6c0da730de4866844c2e4dbf909
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-08 09:57:29 +0530

    Permutations using recursion

--- src/c10_recursion/strings/Permutations.java
+++ src/c10_recursion/strings/Permutations.java
@@ -2,9 +2,14 @@
 
 package c10_recursion.strings;
 
+import java.util.ArrayList;
+
 public class Permutations {
 	public static void main(String[] args) {
-		permutations("", "abc");
+//		permutations("", "abc");
+
+		ArrayList<String> ans = permutationsList("", "abc");
+		System.out.println(ans);
 	}
 
 	static void permutations(String p, String up) {
@@ -20,6 +25,25 @@ public class Permutations {
 			permutations(f + ch + s, up.substring(1));
 		}
 
+	}
+
+	static ArrayList<String> permutationsList(String p, String up) {
+		if (up.isEmpty()) {
+			ArrayList<String> list = new ArrayList<>();
+			list.add(p);
+			return list;
+		}
+		char ch = up.charAt(0);
+
+		// local to this call
+		ArrayList<String> ans = new ArrayList<>();
+
+		for (int i = 0; i <= p.length(); i++) {
+			String f = p.substring(0, i);
+			String s = p.substring(i, p.length());
+			ans.addAll(permutationsList(f + ch + s, up.substring(1)));
+		}
+		return ans;
 	}
 
 }
