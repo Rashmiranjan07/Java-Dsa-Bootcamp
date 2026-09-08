f4035aaf8ae92759315057da18a2ae82c62967c0
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-08 09:52:21 +0530

    Permutations

--- src/c10_recursion/strings/Permutations.java
+++ src/c10_recursion/strings/Permutations.java
@@ -1,12 +1,24 @@
-/*
-Permutation- the element positions can be at multiple positions 
-
-*/
+//Permutation- the element positions can be at multiple positions 
 
 package c10_recursion.strings;
 
 public class Permutations {
 	public static void main(String[] args) {
+
+	}
+
+	static void permutations(String p, String up) {
+		if (up.isEmpty()) {
+			System.out.println(p);
+			return;
+		}
+
+		char ch = up.charAt(0);
+		for (int i = 0; i <= p.length(); i++) {
+			String f = p.substring(0, i);
+			String s = p.substring(i, p.length());
+			permutations(f + ch + s, up.substring(1));
+		}
 
 	}
 
