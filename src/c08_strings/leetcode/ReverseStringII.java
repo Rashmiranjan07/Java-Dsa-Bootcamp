52169d518a9fe2b177659271503f0491dffed404
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-09 20:35:30 +0530

    Reverse String II

--- src/c08_strings/leetcode/ReverseStringII.java
+++ src/c08_strings/leetcode/ReverseStringII.java
@@ -1,13 +1,29 @@
-/*
+/* 541. Reverse String II (Easy)
+--------------------------------
+Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
+If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
+
+Example 1:
+Input: s = "abcdefg", k = 2
+Output: "bacdfeg"
+
+Example 2:
+Input: s = "abcd", k = 2
+Output: "bacd"
  
  */
 
 package c08_strings.leetcode;
 
+import java.util.Arrays;
+
 public class ReverseStringII {
 	public static void main(String[] args) {
 		String s = "abcdef";
 		int k = 2;
+		ReverseStringII obj = new ReverseStringII();
+		String result = obj.reverseStr(s, k);
+		System.out.println(result);
 	}
 
 	public String reverseStr(String s, int k) {
