b52341fd768f041e3239e2ffdaa9a92d0de81f7e
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-09 20:29:15 +0530

    Reverse String II

--- src/c08_strings/leetcode/ReverseStringII.java
+++ src/c08_strings/leetcode/ReverseStringII.java
@@ -2,7 +2,8 @@ package c08_strings.leetcode;
 
 public class ReverseStringII {
 	public static void main(String[] args) {
-
+		String s = "abcdef";
+		int k = 2;
 	}
 
 	public String reverseStr(String s, int k) {
