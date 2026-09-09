526d0652d7ad8569a8ebbf9a3e906cd4f9e90fee
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-09 11:47:56 +0530

    Same Tree

--- src/c05_arrays/SameTree.java
+++ src/c05_arrays/SameTree.java
@@ -23,14 +23,39 @@ import javax.swing.tree.TreeNode;
 
 public class SameTree {
 	public static void main(String[] args) {
-		TreeNode p = { 1, 2, 3 };
-		TreeNode q = { 1, 2, 3 };
+		 int val;
+		    TreeNode left;
+		    TreeNode right;
+
+		    TreeNode() {
+		    }
+
+		    TreeNode(int val) {
+		        this.val = val;
+		    }
+
+		    TreeNode(int val, TreeNode left, TreeNode right) {
+		        this.val = val;
+		        this.left = left;
+		        this.right = right;
+		    }
 	}
 
 	public boolean isSameTree(TreeNode p, TreeNode q) {
 		if (p == null && q == null) {
 			return true;
 		}
+
+		if (p == null || q == null) {
+			return false;
+		}
+
+		if (p.val != q.val) {
+			return false;
+		}
+
+		// Compare left and right subtrees
+		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 	}
 
 }
