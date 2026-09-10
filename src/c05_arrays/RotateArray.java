abe863bdc84ab4a71af214dfbc48423969614c91
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-10 19:16:55 +0530

    Rotate array

--- src/c05_arrays/RotateArray.java
+++ src/c05_arrays/RotateArray.java
@@ -24,10 +24,15 @@ package c05_arrays;
 
 public class RotateArray {
 	public static void main(String[] args) {
+		int[] nums= {1,2,3,4,5,6,7};
+		int k=3; 
 
 	}
 
 	public void rotate(int[] nums, int k) {
+		
+		
+		
 
 	}
 
