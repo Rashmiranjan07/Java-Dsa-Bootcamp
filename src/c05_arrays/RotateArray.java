45c5a8bd696eda7af602a9e40491b5ca40a488d9
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-10 19:29:19 +0530

    Rotate array

--- src/c05_arrays/RotateArray.java
+++ src/c05_arrays/RotateArray.java
@@ -23,28 +23,59 @@ package c05_arrays;
 
 public class RotateArray {
 	public static void main(String[] args) {
+
 		int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
 		int k = 3;
+
 		RotateArray obj = new RotateArray();
 		obj.rotate(nums, k);
-	}
 
-	public void rotate(int[] nums, int k) {
-
-		for (int r = 1; r <= k; r++) {
-
-			int last = nums[nums.length - 1];
-
-			for (int i = nums.length - 1; i > 0; i--) {
-				nums[i] = nums[i - 1];
-			}
-
-			nums[0] = last;
-		}
-
+		// Print result
 		for (int x : nums) {
 			System.out.print(x + " ");
 		}
+	}
+
+	public void rotate(int[] nums, int k) {
+// this way we get time limit exceed 
+//		for (int r = 1; r <= k; r++) {
+//
+//			int last = nums[nums.length - 1];
+//
+//			for (int i = nums.length - 1; i > 0; i--) {
+//				nums[i] = nums[i - 1];
+//			}
+//
+//			nums[0] = last;
+//		}
+//
+//		for (int x : nums) {
+//			System.out.print(x + " ");
+//		}
+
+		int n = nums.length;
+		
+		if (n == 0) {
+			return;
+		}
+		
+		k = k % n;
+
+		reverse(nums, 0, n - 1);
+		reverse(nums, 0, k - 1);
+		reverse(nums, k, n - 1);
+	}
+
+	private void reverse(int[] nums, int left, int right) {
+		while (left < right) {
+			int temp = nums[left];
+			nums[left] = nums[right];
+			nums[right] = temp;
+
+			left++;
+			right--;
+		}
+
 	}
 
 }
