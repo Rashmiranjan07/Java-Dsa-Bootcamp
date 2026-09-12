5434c420e5842bd7fb4381f741343a98baa2b9fd
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-12 18:58:51 +0530

    Reverse String

--- src/c08_strings/leetcode/ReverseString.java
+++ src/c08_strings/leetcode/ReverseString.java
@@ -3,7 +3,10 @@ package c08_strings.leetcode;
 public class ReverseString {
 	public static void main(String[] args) {
 		char[] s = { 'h', 'e', 'l', 'l', 'o' };
-
+		ReverseString obj = new ReverseString();
+		obj.reverseString(s);
+		// Print the reversed array
+		System.out.println(s);
 	}
 
 	public void reverseString(char[] s) {
