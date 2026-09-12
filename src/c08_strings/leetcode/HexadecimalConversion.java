7a4c3c00ceeba307f8b3c86f45ca2c9a344b4f5c
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-12 19:27:27 +0530

    hexadecimal conversion

--- src/c08_strings/leetcode/HexadecimalConversion.java
+++ src/c08_strings/leetcode/HexadecimalConversion.java
@@ -3,10 +3,15 @@ package c08_strings.leetcode;
 public class HexadecimalConversion {
 	public static void main(String[] args) {
 		int num = 26;
+		HexadecimalConversion obj = new HexadecimalConversion();
+		String result = obj.toHex(num);
+		System.out.println(result);
 	}
 
 	public String toHex(int num) {
+		String hex = Integer.toHexString(num);
 
+		return hex;
 	}
 
 }
