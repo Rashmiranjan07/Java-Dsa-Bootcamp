ea7bc1d1eac27849f7e7a8ca9f43061a5ddf1ce6
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-13 11:16:03 +0530

    Shifting Letters

--- src/c08_strings/leetcode/ShiftingLetters.java
+++ src/c08_strings/leetcode/ShiftingLetters.java
@@ -2,7 +2,11 @@ package c08_strings.leetcode;
 
 public class ShiftingLetters {
 	public static void main(String[] args) {
-		
+
+	}
+
+	public String shiftingLetters(String s, int[] shifts) {
+
 	}
 
 }
