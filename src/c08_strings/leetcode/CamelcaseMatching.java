1b45315652a4c862d08787cd878aa93d4e308712
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-14 14:00:33 +0530

    Camelcase Matching

--- src/c08_strings/leetcode/CamelcaseMatching.java
+++ src/c08_strings/leetcode/CamelcaseMatching.java
@@ -1,7 +1,11 @@
 package c08_strings.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CamelcaseMatching {
 	public static void main(String[] args) {
+		String[] queries= {"FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"};
 
 	}
 
