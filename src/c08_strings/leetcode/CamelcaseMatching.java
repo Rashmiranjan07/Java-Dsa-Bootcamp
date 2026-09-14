285ccbc70b6319cf9a94e6b9a2ebb5756150fc53
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-14 13:59:52 +0530

    Camelcase Matching

--- src/c08_strings/leetcode/CamelcaseMatching.java
+++ src/c08_strings/leetcode/CamelcaseMatching.java
@@ -2,7 +2,36 @@ package c08_strings.leetcode;
 
 public class CamelcaseMatching {
 	public static void main(String[] args) {
-		
+
+	}
+
+	public List<Boolean> camelMatch(String[] queries, String pattern) {
+		List<Boolean> result = new ArrayList<>();
+
+		for (String query : queries) {
+			result.add(matches(query, pattern));
+		}
+
+		return result;
+	}
+
+	private boolean matches(String query, String pattern) {
+		int j = 0;
+
+		for (char ch : query.toCharArray()) {
+
+			// Match pattern character
+			if (j < pattern.length() && ch == pattern.charAt(j)) {
+				j++;
+			}
+			// Extra uppercase character is not allowed
+			else if (Character.isUpperCase(ch)) {
+				return false;
+			}
+		}
+
+		// All pattern characters must be matched
+		return j == pattern.length();
 	}
 
 }
