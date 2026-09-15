5c148950e52c0b34ad92241cfc82a471a7a23e70
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-15 12:29:13 +0530

    String to Integer (atoi)

--- src/c08_strings/leetcode/StringToInt.java
+++ src/c08_strings/leetcode/StringToInt.java
@@ -91,13 +91,22 @@ public class StringToInt {
 			i++;
 		}
 
-		// 3. converts digits
-
+		// Convert digits
 		while (i < s.length() && Character.isDigit(s.charAt(i))) {
+
 			int digit = s.charAt(i) - '0';
+
+			// Check overflow
+			if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && digit > 7)) {
+
+				return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+			}
+
 			result = result * 10 + digit;
+
 			i++;
 		}
+
 		return result * sign;
 	}
 }
