27aadac9e85c08bde72dedfedb0972ba848727df
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-16 10:17:50 +0530

    Is Subsequence

--- src/c07_sorting/leetcode/IsSubsequence.java
+++ src/c07_sorting/leetcode/IsSubsequence.java
@@ -2,8 +2,8 @@ package c07_sorting.leetcode;
 
 public class IsSubsequence {
 	public static void main(String[] args) {
-String s="abc";
-String t="";
+		String s = "abc";
+		String t = "ahbgdc";
 	}
 
 	public boolean isSubsequence(String s, String t) {
