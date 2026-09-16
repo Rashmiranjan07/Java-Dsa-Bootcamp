3a33f9948e650c55e4be742fc5f7ac290d81dab7
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-16 11:23:55 +0530

    Largest Number

--- src/c07_sorting/leetcode/LargestNumber.java
+++ src/c07_sorting/leetcode/LargestNumber.java
@@ -15,13 +15,39 @@ Output: "9534330"
 
 package c07_sorting.leetcode;
 
+import java.util.Arrays;
+
 public class LargestNumber {
 	public static void main(String[] args) {
 		int nums[] = { 10, 2 };
+		LargestNumber obj = new LargestNumber();
+		String result = obj.largestNumber(nums);
+		System.out.println(result);
 	}
 
 	public String largestNumber(int[] nums) {
+		String[] arr = new String[nums.length];
 
+		// convert int[] to string[]
+		for (int i = 0; i < nums.length; i++) {
+			arr[i] = String.valueOf(nums[i]);
+		}
+
+		// Custom sorting
+		Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+
+		// If the largest number is 0
+		if (arr[0].equals("0")) {
+			return "0";
+		}
+		// Build answer
+		StringBuilder ans = new StringBuilder();
+
+		for (String s : arr) {
+			ans.append(s);
+		}
+
+		return ans.toString();
 	}
 
 }
