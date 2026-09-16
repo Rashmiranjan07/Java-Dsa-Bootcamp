f42b47131ca5192b4a22e52d625ea5c880997b80
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-16 10:22:14 +0530

    Is Subsequence

--- src/c07_sorting/leetcode/IsSubsequence.java
+++ src/c07_sorting/leetcode/IsSubsequence.java
@@ -7,6 +7,23 @@ public class IsSubsequence {
 	}
 
 	public boolean isSubsequence(String s, String t) {
+		if (t.length() < 0) {
+			return false;
+		}
+
+		int j = 0;
+
+		for (int i = 0; i < t.length() && j < s.length(); i++) {
+			if (t.charAt(i) == s.charAt(j)) {
+				j++;
+			}
+		}
+
+		if (j == s.length()) {
+			return true;
+		}
+
+		return false;
 
 	}
 
