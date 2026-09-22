dc39713bb8452721b39bfd9ec3061f533db75610
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-22 21:29:09 +0530

    Day of the Year

--- src/c12_math/questions/dayOfTheYear.java
+++ src/c12_math/questions/dayOfTheYear.java
@@ -2,10 +2,13 @@ package c12_math.questions;
 
 public class dayOfTheYear {
 	public static void main(String[] args) {
-
+		String date = "2019-01-09";
 	}
 
 	public int dayOfYear(String date) {
+		int year = Integer.parseInt(date.substring(0, 4));
+		int month = Integer.parseInt(date.substring(5, 7));
+		int day = Integer.parseInt(date.substring(8, 10));
 
 	}
 
