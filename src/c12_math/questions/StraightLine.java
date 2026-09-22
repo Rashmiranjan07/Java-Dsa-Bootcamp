96f2fcec981ddd055e9bef0e04225a445793f4c8
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-22 21:42:33 +0530

    Check If It Is a Straight Line

--- src/c12_math/questions/StraightLine.java
+++ src/c12_math/questions/StraightLine.java
@@ -3,9 +3,27 @@ package c12_math.questions;
 public class StraightLine {
 	public static void main(String[] args) {
 		int[][] coordinates = { { 1, 2 }, { 2, 3 }, { 3, 4 }, { 4, 5 }, { 5, 6 }, { 6, 7 } };
+		StraightLine obj = new StraightLine();
+		boolean result = obj.checkStraightLine(coordinates);
+		System.out.println(result);
 	}
 
 	public boolean checkStraightLine(int[][] coordinates) {
+		int x0 = coordinates[0][0];
+		int y0 = coordinates[0][1];
+
+		int x1 = coordinates[1][0];
+		int y1 = coordinates[1][1];
+
+		for (int i = 2; i < coordinates.length; i++) {
+			int x = coordinates[i][0];
+			int y = coordinates[i][1];
+			if ((y - y0) * (x1 - x0) != (y1 - y0) * (x - x0)) {
+				return false;
+			}
+		}
+
+		return true;
 
 	}
 
