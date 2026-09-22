30d18048b5a1894294a3847a556bb5cf6687e407
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-22 17:17:53 +0530

    Buddy Strings

--- src/c08_strings/leetcode/BuddyStrings.java
+++ src/c08_strings/leetcode/BuddyStrings.java
@@ -2,7 +2,8 @@ package c08_strings.leetcode;
 
 public class BuddyStrings {
 	public static void main(String[] args) {
-
+		String s = "aa";
+		String goal = "aa";
 	}
 
 	public boolean buddyStrings(String s, String goal) {
