7c56111395c3db469c7c163072d2e42775a2e8ab
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-23 12:30:58 +0530

    Long pressed Name

--- src/c08_strings/leetcode/LongPressedName.java
+++ src/c08_strings/leetcode/LongPressedName.java
@@ -1,3 +1,24 @@
+/* 925. Long Pressed Name (Easy)
+---------------------------------
+Your friend is typing his name into a keyboard. Sometimes, when typing a character c, the key might get long pressed, and the character will be typed 1 or more times.
+
+You examine the typed characters of the keyboard. Return True if it is possible that it was your friends name, with some characters (possibly none) being long pressed.
+
+ 
+
+Example 1:
+
+Input: name = "alex", typed = "aaleex"
+Output: true
+Explanation: 'a' and 'e' in 'alex' were long pressed.
+Example 2:
+
+Input: name = "saeed", typed = "ssaaedd"
+Output: false
+Explanation: 'e' must have been pressed twice, but it was not in the typed output.
+ */
+
+
 package c08_strings.leetcode;
 
 public class LongPressedName {
@@ -8,6 +29,7 @@ public class LongPressedName {
 	}
 
 	public boolean isLongPressedName(String name, String typed) {
+		
 
 	}
 
