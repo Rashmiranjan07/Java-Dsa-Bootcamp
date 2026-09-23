720006b6951f1d6c6aa22239e9a46d1f3bf5f3bb
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-23 12:28:44 +0530

    Long pressed Name

--- src/c08_strings/leetcode/LongPressedName.java
+++ src/c08_strings/leetcode/LongPressedName.java
@@ -2,7 +2,13 @@ package c08_strings.leetcode;
 
 public class LongPressedName {
 	public static void main(String[] args) {
-		
+		String name="alex";
+		String typed ="aaleex";
+
+	}
+
+	public boolean isLongPressedName(String name, String typed) {
+
 	}
 
 }
