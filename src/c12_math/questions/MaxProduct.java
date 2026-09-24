06150c8c60431c353452b8e40f7bc81e224f6cdb
Rashmiranjan07 <heyyrashmiranjan02@gmail.com>
2026-09-24 11:25:40 +0530

    Maximum Product of Two Digits

--- src/c12_math/questions/MaxProduct.java
+++ src/c12_math/questions/MaxProduct.java
@@ -2,20 +2,31 @@ package c12_math.questions;
 
 public class MaxProduct {
 	public static void main(String[] args) {
-		int n = 22;
-
+		int n = 124;
+		MaxProduct obj = new MaxProduct();
+		int result = obj.maxProduct(n);
+		System.out.println(result);
 	}
 
 	public int maxProduct(int n) {
-		int count = 0;
+		String str = String.valueOf(n);
+		int[] arr = new int[str.length()];
 
-		for (int i = 1; i <= n; i++) {
-			if (n % i == 0) {
-				count++;
+		for (int i = 0; i < str.length(); i++) {
+			arr[i] = str.charAt(i) - '0';
+		}
+
+		int max = 0;
+
+		for (int i = 0; i < arr.length - 1; i++) {
+			int product = arr[i] * arr[i + 1];
+
+			if (product > max) {
+				max = product;
 			}
 		}
-		return count;
 
+		return max;
 	}
 
 }
